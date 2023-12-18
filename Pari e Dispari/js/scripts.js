64b4b30c8665d4f8a6887717760031314f9b3aec
Delonteee <davide.gasparotti@tutanota.com>
2023-12-18 17:43:26 +0100

    Pari e Dispari Completato

--- Pari e Dispari/js/scripts.js
+++ Pari e Dispari/js/scripts.js
@@ -1,22 +1,40 @@
 /* 
-        Palidroma
-    Chiedere all'utente di inserire una parola
-    Creare una funzione per capire se la parola inserita è palindroma
+        Pari e Dispari
+    L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    Sommiamo i due numeri
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    Dichiariamo chi ha vinto.
 */
 
-const parola = prompt('Inserisci una parola:');
+let parola = prompt('Pari o dispari? (P o D)');
 console.log('parola', parola, typeof parola);
 
-for (let i = parol.length - 1; i >= 0; i--) {
-    console.log(parola[i]);
+const numeroUser = parseInt(prompt('Inserisci un numero da 1 a 5:'));
+console.log('numeroUser', numeroUser, typeof numeroUser);
 
-    parolaInversa += parola[i];
+const numeroPc = Math.floor(Math.random() * (4) + 1);
+console.log('numeroPc', numeroPc, typeof numeroPc);
+
+const somma = numeroUser + numeroPc;
+console.log('somma', somma, typeof somma);
+
+let pari;
+
+if (somma % 2 == 0) {
+    console.log(somma, 'somma è PARI');
+    pari = true;
+    console.log('pari', pari, typeof pari);
+} else {
+    console.log(somma, 'somma è DISPARI');
+    pari = false;
+    console.log('pari', pari, typeof pari);
 }
 
-console.log('parolaInversa', parolaInversa, typeof parolaInversa)
-
-if (parola === parolaInversa) {
-    alert('Hai inserito una parola palindroma!!')
+if (pari && parola == 'P'){
+    alert('Hai VINTO!!');
+} else if (!pari && parola == 'D'){
+    alert('Hai VINTO!!');
 } else {
-    alert('Non hai inserito una parola palindroma..')
+    alert('Hai PERSO..');
 }
